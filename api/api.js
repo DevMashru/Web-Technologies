b16fe9bd6282bba3b263d7ae93ae0c6a7c0800f2
Dev <devvmashru@protonmail.ch>
2020-12-04 11:33:56 +0530

    Add separate prices and links for the sites

--- api/api.js
+++ api/api.js
@@ -13,22 +13,35 @@ router.post('/api/top', (req, res) => {
 	var d = []
 	mongoose.connect('mongodb://localhost:27017/project', {useNewUrlParser: true, useUnifiedTopology: true });
 	const db = mongoose.connection;
+	var lowestprice, highestprice;
 	db.once('open', () => {
 		db.collection('top').find({}).toArray((err,r) => {
 			if(err) throw err;
 			r.forEach(obj => {
 				if (obj.Amazon && obj.Flipkart) {
-					if (obj.Amazon.price.slice(-1)[0].price < obj.Flipkart.price.slice(-1)[0].price) {
-						d.push({name: obj.Name, price: obj.Amazon.price.slice(-1)[0].price, lowestprice: obj.Amazon.lowestprice, highestprice: obj.Amazon.highestprice, link: obj.Amazon.link})
+					if(obj.Amazon.lowestprice < obj.Flipkart.lowestprice){
+						lowestprice = obj.Amazon.lowestprice;
 					}
-					else
-						d.push({name: obj.Name, price: obj.Flipkart.price.slice(-1)[0].price, lowestprice: obj.Flipkart.lowestprice, highestprice: obj.Flipkart.highestprice, link: obj.Flipkart.link})
+					else{
+						lowestprice = obj.Flipkart.lowestprice;
+					}
+					if(obj.Amazon.highestprice > obj.Flipkart.highestprice){
+						highestprice = obj.Amazon.highestprice;
+					}
+					else{
+						highestprice = obj.Flipkart.highestprice;
+					}
+					d.push({name: obj.Name, Fprice: obj.Flipkart.price.slice(-1)[0].price, Aprice: obj.Amazon.price.slice(-1)[0].price, lowestprice: lowestprice, highestprice: highestprice, Flink: obj.Flipkart.link, Alink: obj.Amazon.link})
 				}
 				else if (obj.Amazon) {
-					d.push({name: obj.Name, price: obj.Amazon.price.slice(-1)[0].price, lowestprice: obj.Amazon.lowestprice, highestprice: obj.Amazon.highestprice, link: obj.Amazon.link})
+						lowestprice = obj.Amazon.lowestprice;
+						highestprice = obj.Amazon.highestprice;
+						d.push({name: obj.Name, Fprice: 'N/A', Aprice: obj.Amazon.price.slice(-1)[0].price, lowestprice: lowestprice, highestprice: highestprice, Alink: obj.Amazon.link})
 				}
 				else if (obj.Flipkart) {
-					d.push({name: obj.Name, price: obj.Flipkart.price.slice(-1)[0].price, lowestprice: obj.Flipkart.lowestprice, highestprice: obj.Flipkart.highestprice, link: obj.Flipkart.link})
+					lowestprice = obj.Flipkart.lowestprice;
+					highestprice = obj.Flipkart.highestprice;
+					d.push({name: obj.Name, Fprice: obj.Flipkart.price.slice(-1)[0].price, Aprice: 'N/A', lowestprice: lowestprice, highestprice: highestprice, Flink: obj.Flipkart.link})
 				}
 				if (d.length === r.length) {
 					fill(d,res)
