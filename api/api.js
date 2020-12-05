2d088b10ba7068782a59e9f0504517366cff0a4d
Dev <devvmashru@protonmail.ch>
2020-12-05 22:56:43 +0530

    Add support for user items

--- api/api.js
+++ api/api.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router()
 
+router.use(cookieParser())
+
 function fill(d,res){
+	res.set('Access-Control-Allow-Credentials', 'true').set('Access-Control-Allow-Origin', 'http://localhost:3000').set('Access-Control-Allow-Methods', 'POST');
 	return res.send(d);
 }
 
@@ -15,38 +18,45 @@ router.post('/api/top', (req, res) => {
 	const db = mongoose.connection;
 	var lowestprice, highestprice;
 	db.once('open', () => {
-		db.collection('top').find({}).toArray((err,r) => {
+		db.collection('userdata').findOne({username: req.cookies.uname}, (err,re) => {
 			if(err) throw err;
-			r.forEach(obj => {
-				if (obj.Amazon && obj.Flipkart) {
-					if(obj.Amazon.lowestprice < obj.Flipkart.lowestprice){
-						lowestprice = obj.Amazon.lowestprice;
-					}
-					else{
-						lowestprice = obj.Flipkart.lowestprice;
-					}
-					if(obj.Amazon.highestprice > obj.Flipkart.highestprice){
-						highestprice = obj.Amazon.highestprice;
-					}
-					else{
-						highestprice = obj.Flipkart.highestprice;
-					}
-					d.push({name: obj.Name, Fprice: obj.Flipkart.price.slice(-1)[0].price, Aprice: obj.Amazon.price.slice(-1)[0].price, lowestprice: lowestprice, highestprice: highestprice, Flink: obj.Flipkart.link, Alink: obj.Amazon.link})
-				}
-				else if (obj.Amazon) {
-						lowestprice = obj.Amazon.lowestprice;
-						highestprice = obj.Amazon.highestprice;
-						d.push({name: obj.Name, Fprice: 'N/A', Aprice: obj.Amazon.price.slice(-1)[0].price, lowestprice: lowestprice, highestprice: highestprice, Alink: obj.Amazon.link})
-				}
-				else if (obj.Flipkart) {
-					lowestprice = obj.Flipkart.lowestprice;
-					highestprice = obj.Flipkart.highestprice;
-					d.push({name: obj.Name, Fprice: obj.Flipkart.price.slice(-1)[0].price, Aprice: 'N/A', lowestprice: lowestprice, highestprice: highestprice, Flink: obj.Flipkart.link})
-				}
-				if (d.length === r.length) {
-					fill(d,res)
-					mongoose.disconnect();
-				}
+			db.collection('top').find({}).toArray((err,r) => {
+				if(err) throw err;
+				re.items.forEach(item => {
+					r.forEach(obj => {
+						if(item === obj.Name){
+							if (obj.Amazon && obj.Flipkart) {
+								if(obj.Amazon.lowestprice < obj.Flipkart.lowestprice){
+									lowestprice = obj.Amazon.lowestprice;
+								}
+								else{
+									lowestprice = obj.Flipkart.lowestprice;
+								}
+								if(obj.Amazon.highestprice > obj.Flipkart.highestprice){
+									highestprice = obj.Amazon.highestprice;
+								}
+								else{
+									highestprice = obj.Flipkart.highestprice;
+								}
+								d.push({name: obj.Name, Fprice: obj.Flipkart.price.slice(-1)[0].price, Aprice: obj.Amazon.price.slice(-1)[0].price, lowestprice: lowestprice, highestprice: highestprice, Flink: obj.Flipkart.link, Alink: obj.Amazon.link})
+							}
+							else if (obj.Amazon) {
+									lowestprice = obj.Amazon.lowestprice;
+									highestprice = obj.Amazon.highestprice;
+									d.push({name: obj.Name, Fprice: 'N/A', Aprice: obj.Amazon.price.slice(-1)[0].price, lowestprice: lowestprice, highestprice: highestprice, Alink: obj.Amazon.link})
+							}
+							else if (obj.Flipkart) {
+								lowestprice = obj.Flipkart.lowestprice;
+								highestprice = obj.Flipkart.highestprice;
+								d.push({name: obj.Name, Fprice: obj.Flipkart.price.slice(-1)[0].price, Aprice: 'N/A', lowestprice: lowestprice, highestprice: highestprice, Flink: obj.Flipkart.link})
+							}
+							if (d.length === re.items.length) {
+								fill(d,res)
+								mongoose.disconnect();
+							}
+						}
+					});
+				});
 			});
 		});
 	});
